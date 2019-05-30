3c584a0f8ac183db4af3a85daf2fdaaab598687d
Evan <evanturner2018@gmail.com>
2019-05-30 11:10:12 -0400

    dumb animation

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 //runs when document is ready
 $(document).ready(function() {
+    let h = $(window).height();
+    let w = $(window).width();
     
+    //adds an element called front page that will hold content for front page
+    $("#background").append("<div id=frontPage></div>");
+    $("#frontPage").css("background-color","blue")
+        .css("height","0px")
+        .css("width","0px")
+        .css("top",h/2+"px")
+        .css("left",w/2+"px");
+    
+    $("#frontPage").animate({
+        top : "0px",
+        left : "0px",
+        width : w+"px",
+        height : h+"px"
+    });
 });
