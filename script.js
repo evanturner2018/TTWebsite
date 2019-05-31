4f12232436850e83a09ccc3276d0bf568a0645d8
Evan <evanturner2018@gmail.com>
2019-05-31 13:59:07 -0400

    background image

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ $(document).ready(function() {
     let h = $(window).height();
     let w = $(window).width();
 
+    $("#background").append("<img src=\"H_T.jpg\"></img>");
+
     //adds an element called front page that will hold content for front page
     $("#background").append("<div id=\"frontPage\">&Theta;&Tau;</div>");
 
@@ -13,19 +15,23 @@ $(document).ready(function() {
     $("#frontPage").append("<p>Sigma Chapter</p>");
     $("p").css("font-size","25px")
         .css("color", "gray")
-        .css("opacity", "0")
-        .css("top","65px")
+        .css("opacity", "0") //starts invisible, then it'll fade in
+        .css("top","65px") //got this number by guessing
         .css("position", "absolute")
         .css("width", "150px")
-        .css("right", "-10px");
+        .css("right", "-10px"); //idk why -10 but it wasn't centered
 
     $("#frontPage").animate({
         top : "250px",
         opacity : "1"
     }, 1000, function() {
+        //fade in the "sigma chapter" and image together
         $("p").animate({
             opacity : 1,
             top : "75px"
+        }, 1000);
+        $("img").animate({
+            opacity : .1
         }, 1000);
     });
 
